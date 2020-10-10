4c578454a96d024d30f975ec043e04b33f264170
MAJKFL <kubaflor23@gmail.com>
2020-10-10 16:31:45 +0200

    Initial Commit

---
+++ Classmate/ClassmateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassmateApp.swift
+//  Classmate
+//
+//  Created by Kuba Florek on 10/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct ClassmateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
